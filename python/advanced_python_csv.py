71518dd8e64fb2ff28c31e1352ec3d86c78053b0
Laura Fedoruk <laurafedoruk@Lauras-MacBook-Air-2.local>
2017-06-21 12:33:29 -0700

    added example code to advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,13 @@
-PLACE YOUR CODE HERE
+import sys
+import os
+import csv
+
+#new example code vs. problem code
+
+list_of_emails = ['laura.fedoruk@gmail.com', 'andrew.young28@gmail.com', 'fedoruks@shaw.ca', 'test@yahoo.com']
+
+with open ('emails.csv', 'w') as csv_file:
+    emailwriter = csv.writer(csv_file)
+    emailwriter.writerow(['list_of_emails'])
+    for email in list_of_emails:
+        emailwriter.writerow(['%s' % email])
